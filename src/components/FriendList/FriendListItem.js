9567666d00fde7e76af59419f3323e3966aaba7e
cid <ilya.gorbachevskiy@gmail.com>
2020-02-01 17:19:56 +0200

    fix frienditem

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './FriendList.module.css';
 
-function FriendListItem({id, avatar, name, isOnline}) {    
+function FriendListItem({avatar, name, isOnline}) {    
     return (
-        <li key={id} className={styles.item}>
+        <li className={styles.item}>
             <span className={isOnline ? styles.online : styles.ofline}></span>
             <img className={styles.avatar} src={avatar} alt="" width="48" />
             <p className={styles.name}>{name}</p>
