fc3ea5760e3ea7748f2b59f9296a54e2533ec915
Alex Shynkarenko <shynkarenkoalex5@gmail.com>
2020-02-02 16:39:16 +0200

    Updates

---
+++ precache-manifest.38ebb85296896320e362818573ece6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6b78380dd7865e3a9c8b1ade7fef1d",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5e8b0955075dbd41d9a5",
+    "url": "/goit-react-hw-01-components/static/css/main.7781060c.chunk.css"
+  },
+  {
+    "revision": "1224a5e0e7d8ff04f4dc",
+    "url": "/goit-react-hw-01-components/static/js/2.25b66531.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.25b66531.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e8b0955075dbd41d9a5",
+    "url": "/goit-react-hw-01-components/static/js/main.b9b8926e.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
